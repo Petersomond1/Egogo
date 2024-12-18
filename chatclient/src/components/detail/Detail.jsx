357b27fde4416c496d756e31d887b261e4ca47d9
Petersomond1 <petersomond@gmail.com>
2024-12-18 17:01:12 -0500

    details 1

--- chatclient/src/components/detail/Detail.jsx
+++ chatclient/src/components/detail/Detail.jsx
@@ -3,7 +3,50 @@ import './detail.css'
 
 const Detail = () => {
     return (
-        <div className="detail_container"> detail </div>
+        <div className="detail_container">
+            <div className="user">
+                <img src="" alt="" />
+                <h2>Robert King</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing!
+                </p>
+            </div>
+            <div className="info">
+                <div className="option">
+                <div className="title">
+                <span>Chat Settings</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>
+                </div>
+                <div className="option">
+                <div className="title">
+                <span>Privacy & help</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>
+                </div>
+                <div className="option">
+                <div className="title">
+                <span>Shared Photos</span>
+                <img src="./arrowDown.png" alt="" />
+                </div>
+                <div className="photos">
+                    <div className="photoItem">
+                        <img src="./image.png" alt="" />
+                        <span>photo_name_2024</span>
+                    </div>
+                    <img src="./download.png" alt="icon" />
+                </div>
+                </div>
+                <div className="option">
+                <div className="title">
+                <span>Shared Files</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>
+                </div>
+                <button>Block User</button>
+               
+            </div>
+        </div>
     )
 }
 
