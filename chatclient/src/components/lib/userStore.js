a9d5bbf845a115e29b4148030bd2e0bb2eee37f6
Petersomond1 <petersomond@gmail.com>
2024-12-21 11:40:37 -0500

    Debug done. useStore logic used to get user data visible. we provided the ref to the document and used the fxn getDoc to get a snap of it called document snap.

--- chatclient/src/components/lib/userStore.js
+++ chatclient/src/components/lib/userStore.js
@@ -4,6 +4,20 @@ import { doc, getDoc } from 'firebase/firestore'
 
 
 //Here we use the "doc getDoc" function to get the user data from the firestore database.
+//We use the "create" function from zustand to create a store that will hold the user data.
+//We use the "set" function to update the store with the user data.
+//We use the "fetchUserInfo" function to fetch the user data from the firestore database.
+//We use the "currentUser" and "isLoading" variables to store the user data and loading state respectively.
+//We use the "useUserStore" hook to access the user data and loading state in our components.
+//We use the "useEffect" hook to fetch the user data when the component mounts.
+//We use the "onAuthStateChanged" function to listen for changes in the user authentication state.
+//We use the "unSub" function to unsubscribe from the user authentication state changes.
+//We use the "currentUser" variable to conditionally render the chat or login page based on the user authentication state.
+//We use the "isLoading" variable to show a loading indicator while fetching the user data.
+//We use the "Notification" component to show a loading indicator while fetching the user data.
+//We use the "currentUser" variable to conditionally render the chat or login page based on the user authentication state.
+
+
 export const useUserStore = create((set) => ({
   currentUser: null,
     isLoading: true,
@@ -12,19 +26,19 @@ export const useUserStore = create((set) => ({
 
     try {
 
-        const docRef = doc(db, "users", "uid");   // called a reference to the document
+        const docRef = doc(db, "users", uid);   // called a reference to the document
         const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
         set({ currentUser: docSnap.data(), isLoading: false });
-        // console.log("Document data:", docSnap.data());
+        console.log("Document data:", docSnap.data());
     }else{
         set({ currentUser: null, isLoading: false });
-        // console.log("No such document!");
+        console.log("No such document!");
     }    
     } catch (err) {
         console.log(err);
-        return set({ currentUser: null, isLoading: false });
+         set({ currentUser: null, isLoading: false });
         
     }
 
