e1828c584c5c57e0dba0c1e387435467c40e13e4
Petersomond1 <petersomond@gmail.com>
2024-12-21 16:29:47 -0500

    now to fetch realtime data and display as realtime chats data. we start with chatlist.jsx. deleted all the construction chat list but one and used it to map data on chat storage of current user. After the promises, we create our first chat using plus-button of upload or search at main chat page and addUser

--- chatclient/src/components/list/chatlist/Chatlist.jsx
+++ chatclient/src/components/list/chatlist/Chatlist.jsx
@@ -1,11 +1,37 @@
-import {react, useState} from 'react';
+import {react, useEffect, useState} from 'react';
 import './chatlist.css';
 import AddUser from './addUser/AddUser';
+import {useUserStore} from '../../lib/userStore';
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from '../../lib/firebase';
 
 const Chatlist = () => {
     const [addMode, setAddMode] = useState(false)
+    const [chats, setChats] = useState([]);
 
-    
+    const {currentUser} = useUserStore();
+
+    useEffect(() => {
+        // doc becomes res
+                    const unsub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
+                        console.log("Current data: ", doc.data());
+                        const items = res.data().chats;
+
+                      //  setChats(doc.data());
+
+                      const promises = items.map(async item => {
+                          const userDocRef = doc(db, "users", item.receiverId);  // receiverId first introduced. it is the id of the user the current user is chatting with.
+                            const userDocSnap = await getDoc(userDocRef);
+
+                            const user = userDocSnap.data();
+
+                            return {...items, receiver: user};
+                      });
+                      const chatData = await Promise.all(promises);
+                        setChats(chatData).sort((a, b)=> b.updatedAt - a.UpdatedAt);         // setChats(doc.data()); and you sort() the chats array by the lastMessage timestamp.
+                    }); 
+                    return () => unsub();
+    }, [currentUser.id])
     return (
         <div className="chatlist_container"> 
         <div className="search">
@@ -17,70 +43,24 @@ const Chatlist = () => {
             onClick={() => setAddMode(!addMode)}
              />
         </div>
-        <div className="item">
+
+        {chats.map(chat => (
+
+        <div className="item" key={chat.chatId}>
             <img src="./avatar.png" alt="" />
             <div className="texts">
-                <h2>mond pete</h2>
+                <h2>Monday P</h2>
                 <p>hello</p>
+                <p>12:00</p>
+
+                <p>{chat.lastMessage}</p>
             </div>
-            <p>12:00</p>
+            <p>13:00</p>
         </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
-        <div className="item">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-                <h2>mond pete</h2>
-                <p>hello</p>
-            </div>
-            <p>12:00</p>
-        </div>
+        ))}
+
+
+        
        {addMode && <AddUser />}
         </div>
     )
@@ -89,3 +69,6 @@ const Chatlist = () => {
 export default Chatlist;
 
 // Addition of the AddUser component to the Chatlist component ends the clientside design of the chat application.
+// The next step is to implement the backend logic of the chat application.
+// The backend logic will be implemented using Node.js, Express.js and Socket.io.
+
--- chatclient/src/components/list/chatlist/addUser/AddUser.jsx
+++ chatclient/src/components/list/chatlist/addUser/AddUser.jsx
@@ -2,9 +2,21 @@ import './AddUser.css';
 
 const AddUser = () => {
 
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+
+        try {                               // we need a condition and we'll need query now querySnapshot to get the data from the database.
+            
+        } catch (err) {
+            
+        }
+    }
     return (
         <div className="addUser">
-            <form>
+            <form onSubmit={handleSearch}>
                 <input type="text" placeholder='Username' name='username' />
                 <button>Search</button>
             </form>
